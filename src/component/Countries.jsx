299fc16d8050968be83943b88928cada200c8a26
khanmahfuj34 <mkmahfujkhanms3@gmail.com>
2026-03-01 01:09:10 +0600

    Fix template literal error and update country component

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -1,19 +1,26 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './country/country';
 import './countries.css';
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries]=useState([]);
+    const handleVisitedCountries= (country)=>{
+        console.log('handel Visited country clicked',country);
+    }
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
     
     return (
-        <div className='countries'>
+        <>
             <h2>Here is the data: {countries.length}</h2>
-            {
-                countries.map(country=><Country country={country}></Country>)
+            <h3>Total Country Visited: </h3>
+           <div className='countries'>
+             {
+                countries.map(country=><Country country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
             }
-        </div>
+           </div>
+        </>
     );
 };
 
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
 
-const Country = ({country}) => {
-    console.log(country)
-    const handelVisited = ()=>{
-        console.log('btn clicked')
+const Country = ({country, handleVisitedCountries}) => {
+    const [visited, setVisited] = useState(false);
+    // console.log(handleVisitedCountries)
+    const handleVisited = ()=>{
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+        // setVisited(visited? false:true);
+        setVisited(!visited);
+        handleVisitedCountries(country)
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags.png} alt="" />
             <h3>Name: {country?.name?.common}</h3>
             <p>Population: {country?.population?.population}</p>
             <p>Area: {country?.area?.area} {country?.area?.area > 30000 ? "Big Country" : "small Country"}</p>
-            <button onClick={handelVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited? "Visited": "Not Visited"}
+            </button>
         </div>
     );
 };
